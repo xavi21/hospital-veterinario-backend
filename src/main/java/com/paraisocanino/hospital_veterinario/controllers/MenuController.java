2957d830201f34a6e2e7f30990aafb01e0ba23e6
Javier Salazar <javfa1992@gmail.com>
2024-07-29 23:24:23 -0600

    crud menu

--- src/main/java/com/paraisocanino/hospital_veterinario/controllers/MenuController.java
+++ src/main/java/com/paraisocanino/hospital_veterinario/controllers/MenuController.java
@@ -1,17 +1,122 @@
 package com.paraisocanino.hospital_veterinario.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.paraisocanino.hospital_veterinario.models.Menu;
+import com.paraisocanino.hospital_veterinario.models.Sucursal;
+import com.paraisocanino.hospital_veterinario.payload.response.GeneralResponse;
+import com.paraisocanino.hospital_veterinario.payload.response.GeneralResponseList;
+import com.paraisocanino.hospital_veterinario.repository.MenuRepository;
+import com.paraisocanino.hospital_veterinario.repository.SucursalRepository;
+import com.paraisocanino.hospital_veterinario.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/menu")
 public class MenuController {
 
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
     @GetMapping("/all")
-    public String menus() {
-        return "Public Content.";
+    public GeneralResponseList getMenus(@RequestHeader("Authorization") String tokenAdmin) {
+        List<Menu> menus = menuRepository.findAll();
+
+        List<Menu> menuResponse = new ArrayList<>();
+
+        for (Menu menu : menus) {
+            Menu menur = new Menu();
+            menur.setIdmenu(menu.getIdmenu());
+            menur.setName(menu.getName());
+            menur.setOrdenmenu(menu.getOrdenmenu());
+            menur.setUsuariocreacion(menu.getUsuariocreacion());
+            menur.setFechacreacion(menu.getFechacreacion());
+            menur.setUsuariomodificacion(menu.getUsuariomodificacion());
+            menur.setFechamodificacion(menu.getFechamodificacion());
+
+            menuResponse.add(menur);
+        }
+
+        final GeneralResponseList response = new GeneralResponseList();
+        response.setCode(200);
+        response.setMessage("Exitoso");
+        response.setData(menuResponse);
+
+        return response;
+    }
+
+    @PostMapping("/save")
+    public GeneralResponse postMenu(@RequestBody Menu menu, @RequestHeader("Authorization") String tokenAdmin) {
+
+        tokenAdmin = tokenAdmin.replace("Bearer ", "");
+        final String user = jwtUtils.getUserNameFromJwtToken(tokenAdmin);
+        menu.setFechamodificacion(dateNow());
+        menu.setUsuariomodificacion(user);
+        menu.setUsuariocreacion(user);
+        menu.setFechacreacion(dateNow());
+
+        menuRepository.save(menu);
+        final GeneralResponse response = new GeneralResponse();
+        response.setCode(200);
+        response.setMessage("Datos Guardados");
+        response.setData(menu);
+
+        return response;
+    }
+
+    @PutMapping("/update")
+    public GeneralResponse putMenu(@RequestBody Menu menu, @RequestHeader("Authorization") String tokenAdmin) {
+
+        tokenAdmin = tokenAdmin.replace("Bearer ", "");
+        final String user = jwtUtils.getUserNameFromJwtToken(tokenAdmin);
+        final GeneralResponse response = new GeneralResponse();
+
+        if (menuRepository.findById(menu.getIdmenu()).isPresent()) {
+
+            Optional<Menu> currenMenu = menuRepository.findById(menu.getIdmenu());
+
+            menu.setFechamodificacion(dateNow());
+            menu.setUsuariomodificacion(user);
+            menu.setUsuariocreacion(currenMenu.get().getUsuariocreacion());
+            menu.setFechacreacion(currenMenu.get().getFechacreacion());
+            menuRepository.save(menu);
+
+            response.setCode(200);
+            response.setMessage("Menu Actualizada");
+        } else {
+            response.setCode(401);
+            response.setMessage("Error al actualizar");
+        }
+        response.setData(menu);
+        return response;
+
+    }
+
+    @DeleteMapping(path = "/eliminar")
+    public GeneralResponse EliminaProducto(@RequestParam Integer id) {
+        final GeneralResponse response = new GeneralResponse();
+        Optional<Menu> currenMenu = menuRepository.findById(id);
+        if (currenMenu.isPresent()) {
+            menuRepository.delete(currenMenu.get());
+            response.setCode(200);
+            response.setMessage("Se eliminó el menu : " + id);
+
+        } else {
+            response.setCode(200);
+            response.setMessage("No existe el menu :" + id);
+        }
+        return response;
+    }
+
+    private LocalDate dateNow() {
+        return LocalDate.now();
     }
 }
--- src/main/java/com/paraisocanino/hospital_veterinario/models/Menu.java
+++ src/main/java/com/paraisocanino/hospital_veterinario/models/Menu.java
@@ -4,20 +4,23 @@ import com.paraisocanino.hospital_veterinario.payload.Bitacora;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "menu")
 public class Menu  extends Bitacora {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "menu", strategy = "enhanced-table", parameters = {
+            @org.hibernate.annotations.Parameter(name = "menu", value = "sequence_menu") })
+    @GeneratedValue(generator = "secuence_menu", strategy = GenerationType.TABLE)
+    @SequenceGenerator(name = "secuence_menu", allocationSize=1)
     private Integer idmenu;
 
     @NotBlank
     @Size(max = 50)
     private String name;
 
-    @NotBlank
     private Integer ordenmenu;
 
 
