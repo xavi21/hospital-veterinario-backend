d37ee08e05f0a84466580c2db74a65bd58388db2
Javier Salazar <javfa1992@gmail.com>
2024-09-07 09:55:39 -0600

    Actualizar persona

--- src/main/java/com/paraisocanino/hospital_veterinario/controllers/PersonaController.java
+++ src/main/java/com/paraisocanino/hospital_veterinario/controllers/PersonaController.java
@@ -1,6 +1,7 @@
 package com.paraisocanino.hospital_veterinario.controllers;
 
 import com.paraisocanino.hospital_veterinario.models.Persona;
+import com.paraisocanino.hospital_veterinario.models.Talla;
 import com.paraisocanino.hospital_veterinario.payload.response.GeneralResponse;
 import com.paraisocanino.hospital_veterinario.payload.response.GeneralResponseList;
 import com.paraisocanino.hospital_veterinario.repository.PersonaRepository;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -74,6 +76,35 @@ public class PersonaController {
 
         return response;
     }
+
+    @PutMapping("/update")
+    public GeneralResponse updatePersona(@RequestBody Persona persona, @RequestHeader("Authorization") String tokenAdmin) {
+
+        tokenAdmin = tokenAdmin.replace("Bearer ", "");
+        final String user = jwtUtils.getUserNameFromJwtToken(tokenAdmin);
+        final GeneralResponse response = new GeneralResponse();
+
+        Optional<Persona> currentPersona = personaRepository.findById(persona.getIdpersona());
+
+        if (currentPersona.isPresent()) {
+
+            persona.setFechamodificacion(dateNow());
+            persona.setUsuariomodificacion(user);
+            persona.setUsuariocreacion(currentPersona.get().getUsuariocreacion());
+            persona.setFechacreacion(currentPersona.get().getFechacreacion());
+            personaRepository.save(persona);
+
+            response.setCode(200);
+            response.setMessage("Persona Actualizada");
+        } else {
+            response.setCode(401);
+            response.setMessage("Error al actualizar");
+        }
+
+        response.setData(persona);
+        return response;
+    }
+
 
     private LocalDate dateNow() {
         return LocalDate.now();
