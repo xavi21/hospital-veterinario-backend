3cd8b1e8b734ab3b6833c14bed8030dc63e6ee9b
Javier Salazar <javfa1992@gmail.com>
2024-09-11 18:49:38 -0600

    Actualizar consulta

--- src/main/java/com/paraisocanino/hospital_veterinario/controllers/ConsultaController.java
+++ src/main/java/com/paraisocanino/hospital_veterinario/controllers/ConsultaController.java
@@ -1,7 +1,9 @@
 package com.paraisocanino.hospital_veterinario.controllers;
 
 
+import com.paraisocanino.hospital_veterinario.models.Cita;
 import com.paraisocanino.hospital_veterinario.models.Consulta;
+import com.paraisocanino.hospital_veterinario.models.Persona;
 import com.paraisocanino.hospital_veterinario.payload.response.GeneralResponse;
 import com.paraisocanino.hospital_veterinario.payload.response.GeneralResponseList;
 import com.paraisocanino.hospital_veterinario.repository.ConsultaProjection;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -52,6 +55,34 @@ public class ConsultaController {
         response.setMessage("Datos Guardados");
         response.setData(consulta);
 
+        return response;
+    }
+
+    @PutMapping("/update")
+    public GeneralResponse updateConsulta(@RequestBody Consulta consulta, @RequestHeader("Authorization") String tokenAdmin) {
+
+        tokenAdmin = tokenAdmin.replace("Bearer ", "");
+        final String user = jwtUtils.getUserNameFromJwtToken(tokenAdmin);
+        final GeneralResponse response = new GeneralResponse();
+
+        Optional<Consulta> currentConsulta = consultaRepository.findById(consulta.getIdconsulta());
+
+        if (currentConsulta.isPresent()) {
+
+            consulta.setFechamodificacion(dateNow());
+            consulta.setUsuariomodificacion(user);
+            consulta.setUsuariocreacion(currentConsulta.get().getUsuariocreacion());
+            consulta.setFechacreacion(currentConsulta.get().getFechacreacion());
+            consultaRepository.save(consulta);
+
+            response.setCode(200);
+            response.setMessage("Consulta Actualizada");
+        } else {
+            response.setCode(401);
+            response.setMessage("Error al actualizar");
+        }
+
+        response.setData(consulta);
         return response;
     }
 
